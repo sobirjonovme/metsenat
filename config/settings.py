0c205d70d2c80b1c7304326d7d0326ed4f5f57a2
sobirjonovme <huntsman6004@gmail.com>
2023-02-28 15:35:54 +0500

    created: CustomUser model

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # installed apps
+    'rest_framework',
+
+    # created apps
+    'users',
+
 ]
 
 MIDDLEWARE = [
@@ -121,3 +128,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+###############################################
+###############################################
+
+AUTH_USER_MODEL = 'users.CustomUser'
+
